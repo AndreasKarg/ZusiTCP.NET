0132b0fc87e557a9c565f8ab97241c70fb4ed1a9
AndreasKarg <Clonkman@gmx.de>
2012-10-27 17:18:22 +0200

    Feature: Introduce examples by F. Schn.

---
+++ examples/VB.Net/AKTCPDemo2/AKTCPDemo2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("AKTCPDemo2")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("AKTCPDemo2")> 
+<Assembly: AssemblyCopyright("Copyright ©  2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("4821e34c-8538-4529-b61e-d76f88fb30d5")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ examples/VB.Net/readme_DE.txt
@@ -0,0 +1,3 @@
+In diesem Ordner befinden sich Beispielapplikationen für Visual Basic.NET.
+
+Sie wurden von Nutzer F. Schn. aus dem Zusiforum beigesteuert und unterliegen der selben Lizenz wie die Bibliothek selbst.
