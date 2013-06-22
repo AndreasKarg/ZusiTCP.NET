f456c62705377a139c1757c120694ebe619287d2
Andreas Karg <Clonkman@gmx.de>
2013-06-22 20:21:20 +0200

    WIP: Create duplicate of simple example with stub for WIndsor

---
+++ WindsorInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Zusi_Datenausgabe
+{
+  public class WindsorInstaller : IWindsorInstaller
+  {
+    public void Install(IWindsorContainer container, IConfigurationStore store)
+    {
+      container.Register()
+    }
+  }
+}
