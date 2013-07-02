37d511dc029ea995007ae9be6403d3c0d9b929b1
Andreas Karg <Clonkman@gmx.de>
2013-07-02 17:07:15 +0200

    Cleanup: Cleanup unit test cases for basic black box testing

--- Zusi-Datenausgabe_Test/TypedEventManagerTest.cs
+++ Zusi-Datenausgabe_Test/TypedEventManagerTest.cs
@@ -21,7 +21,7 @@ namespace Zusi_Datenausgabe_Test
     private TestContext _testContextInstance;
     private IWindsorContainer _container;
     private ITypedEventManager _target;
-    private bool _eventHasBeenCalled;
+    private int _eventHasBeenCalled;
 
     /// <summary>
     ///Gets or sets the test context which provides
@@ -62,6 +62,7 @@ namespace Zusi_Datenausgabe_Test
     {
       _container = BootstrapContainer();
       _target = _container.Resolve<ITypedEventManager>();
+      _eventHasBeenCalled = 0;
     }
     //
     //Use TestCleanup to run code after each test has run
@@ -81,145 +82,39 @@ namespace Zusi_Datenausgabe_Test
         );
     }
 
-    /// <summary>
-    ///A test for CastToListType
-    ///</summary>
-    public void CastToListTypeTestHelper<T>()
-    {
-      PrivateObject param0 = null; // TODO: Initialize to an appropriate value
-      TypedEventManager_Accessor target = new TypedEventManager_Accessor(param0); // TODO: Initialize to an appropriate value
-      ITypedMethodListBase handlerList = null; // TODO: Initialize to an appropriate value
-      ITypedMethodList<DataReceivedEventArgs<T>> expected = null; // TODO: Initialize to an appropriate value
-      ITypedMethodList<DataReceivedEventArgs<T>> actual;
-      actual = target.CastToListType<T>(handlerList);
-      Assert.AreEqual(expected, actual);
-      Assert.Inconclusive("Verify the correctness of this test method.");
-    }
-
-    [TestMethod]
-    [DeploymentItem("Zusi-Datenausgabe.dll")]
-    public void CastToListTypeTest()
-    {
-      CastToListTypeTestHelper<GenericParameterHelper>();
-    }
-
-    /// <summary>
-    ///A test for CreateNewList
-    ///</summary>
-    public void CreateNewListTestHelper<T>()
-    {
-      PrivateObject param0 = null; // TODO: Initialize to an appropriate value
-      TypedEventManager_Accessor target = new TypedEventManager_Accessor(param0); // TODO: Initialize to an appropriate value
-      Type type = null; // TODO: Initialize to an appropriate value
-      ITypedMethodList<DataReceivedEventArgs<T>> expected = null; // TODO: Initialize to an appropriate value
-      ITypedMethodList<DataReceivedEventArgs<T>> actual;
-      actual = target.CreateNewList<T>(type);
-      Assert.AreEqual(expected, actual);
-      Assert.Inconclusive("Verify the correctness of this test method.");
-    }
-
-    [TestMethod]
-    [DeploymentItem("Zusi-Datenausgabe.dll")]
-    public void CreateNewListTest()
-    {
-      CreateNewListTestHelper<GenericParameterHelper>();
-    }
-
-    /// <summary>
-    ///A test for GetDictionaryEntry
-    ///</summary>
-    public void GetDictionaryEntryTestHelper<T>()
-    {
-      PrivateObject param0 = null; // TODO: Initialize to an appropriate value
-      TypedEventManager_Accessor target = new TypedEventManager_Accessor(param0); // TODO: Initialize to an appropriate value
-      ITypedMethodList<DataReceivedEventArgs<T>> expected = null; // TODO: Initialize to an appropriate value
-      ITypedMethodList<DataReceivedEventArgs<T>> actual;
-      actual = target.GetDictionaryEntry<T>();
-      Assert.AreEqual(expected, actual);
-      Assert.Inconclusive("Verify the correctness of this test method.");
-    }
-
-    [TestMethod]
-    [DeploymentItem("Zusi-Datenausgabe.dll")]
-    public void GetDictionaryEntryTest()
-    {
-      GetDictionaryEntryTestHelper<GenericParameterHelper>();
-    }
-
-    /// <summary>
-    ///A test for Invoke
-    ///</summary>
-    public void InvokeTestHelper<T>()
-    {
-      IDictionary<Type, ITypedMethodListBase> eventHandlers = null; // TODO: Initialize to an appropriate value
-      ITypedMethodListFactory methodListFactory = null; // TODO: Initialize to an appropriate value
-      TypedEventManager target = new TypedEventManager(eventHandlers, methodListFactory); // TODO: Initialize to an appropriate value
-      object sender = null; // TODO: Initialize to an appropriate value
-      DataReceivedEventArgs<T> eventArgs = null; // TODO: Initialize to an appropriate value
-      target.Invoke<T>(sender, eventArgs);
-      Assert.Inconclusive("A method that does not return a value cannot be verified.");
-    }
-
     [TestMethod]
     public void InvokeTest()
     {
-      InvokeTestHelper<GenericParameterHelper>();
-    }
-
-    /// <summary>
-    ///A test for Subscribe
-    ///</summary>
-    public void SubscribeTestHelper<T>()
-    {
-      IDictionary<Type, ITypedMethodListBase> eventHandlers = null; // TODO: Initialize to an appropriate value
-      ITypedMethodListFactory methodListFactory = null; // TODO: Initialize to an appropriate value
-      TypedEventManager target = new TypedEventManager(eventHandlers, methodListFactory); // TODO: Initialize to an appropriate value
-      EventHandler<DataReceivedEventArgs<T>> handler = null; // TODO: Initialize to an appropriate value
-      target.Subscribe<T>(handler);
-      Assert.Inconclusive("A method that does not return a value cannot be verified.");
+      _target.Subscribe<bool>(BoolEventHandler);
+      _target.Invoke(this, new DataReceivedEventArgs<bool>(123, true));
+      Assert.IsTrue(_eventHasBeenCalled == 1);
     }
 
     [TestMethod]
     public void SubscribeTest()
     {
-      SubscribeTestHelper<GenericParameterHelper>();
-    }
+      _target.Invoke(this, new DataReceivedEventArgs<bool>(123, true));
+      Assert.IsTrue(_eventHasBeenCalled == 0);
 
-    /// <summary>
-    ///A test for TryGetDictionaryEntry
-    ///</summary>
-    public void TryGetDictionaryEntryTestHelper<T>()
-    {
-      PrivateObject param0 = null; // TODO: Initialize to an appropriate value
-      TypedEventManager_Accessor target = new TypedEventManager_Accessor(param0); // TODO: Initialize to an appropriate value
-      Type type = null; // TODO: Initialize to an appropriate value
-      ITypedMethodList<DataReceivedEventArgs<T>> result = null; // TODO: Initialize to an appropriate value
-      ITypedMethodList<DataReceivedEventArgs<T>> resultExpected = null; // TODO: Initialize to an appropriate value
-      bool expected = false; // TODO: Initialize to an appropriate value
-      bool actual;
-      actual = target.TryGetDictionaryEntry<T>(type, out result);
-      Assert.AreEqual(resultExpected, result);
-      Assert.AreEqual(expected, actual);
-      Assert.Inconclusive("Verify the correctness of this test method.");
-    }
+      _target.Subscribe<bool>(BoolEventHandler);
+      _target.Invoke(this, new DataReceivedEventArgs<bool>(123, true));
+      Assert.IsTrue(_eventHasBeenCalled == 1);
 
-    [TestMethod]
-    [DeploymentItem("Zusi-Datenausgabe.dll")]
-    public void TryGetDictionaryEntryTest()
-    {
-      TryGetDictionaryEntryTestHelper<GenericParameterHelper>();
+      _target.Subscribe<bool>(BoolEventHandler);
+      _target.Invoke(this, new DataReceivedEventArgs<bool>(123, true));
+      Assert.IsTrue(_eventHasBeenCalled == 2);
     }
 
     private void GenericEventHandler<T>(object sender, DataReceivedEventArgs<T> data)
     {
-      _eventHasBeenCalled = true;
+      _eventHasBeenCalled++;
       Trace.WriteLine("Event handler for type {0} has been called.", typeof(T).ToString());
     }
 
     private void BoolEventHandler(object sender, DataReceivedEventArgs<bool> shouldHaveBeenCalled)
     {
       Trace.WriteLine("Bool event handler has been called. It should {0}have been.", (shouldHaveBeenCalled.Value) ? "" : "*not* ");
-      _eventHasBeenCalled = true;
+      _eventHasBeenCalled++;
       Assert.IsTrue(shouldHaveBeenCalled.Value);
     }
 
@@ -228,12 +123,12 @@ namespace Zusi_Datenausgabe_Test
     {
       _target.Subscribe<bool>(BoolEventHandler);
       _target.Invoke(this, new DataReceivedEventArgs<bool>(123, true));
-      Assert.IsTrue(_eventHasBeenCalled);
+      Assert.IsTrue(_eventHasBeenCalled == 1);
       _target.Unsubscribe<bool>(BoolEventHandler);
-
-      _eventHasBeenCalled = false;
       _target.Invoke(this, new DataReceivedEventArgs<bool>(123, false));
-      Assert.IsFalse(_eventHasBeenCalled);
+      Assert.IsTrue(_eventHasBeenCalled == 1);
+
+      _target.Unsubscribe<bool>(BoolEventHandler);
     }
   }
 }
