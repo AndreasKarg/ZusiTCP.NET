6cd97c2fd64543550f20bb291c903985299dcfee
Andreas Karg <Clonkman@gmx.de>
2013-07-03 16:37:48 +0200

    Feature-ish: Introduce (unused) generic EventManager class

---
+++ EventManager/EventManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Zusi_Datenausgabe.EventManager
+{
+  internal interface IEventManager<in TKey> : IEventInvoker<TKey>
+  {
+    void Subscribe<T>(TKey key, EventHandler<DataReceivedEventArgs<T>> handler);
+    void Unsubscribe<T>(TKey key, EventHandler<DataReceivedEventArgs<T>> handler);
+  }
+
+  internal interface IEventInvoker<in TKey>
+  {
+    void Invoke<T>(TKey key, object sender, DataReceivedEventArgs<T> eventArgs);
+  }
+
+  class EventManager<TKey> : EventManagerBase<TKey>, IEventManager<TKey>
+  {
+    public EventManager(ITypedMethodListFactory methodListFactory) : base(methodListFactory)
+    {
+    }
+
+    public new void Subscribe<T>(TKey key, EventHandler<DataReceivedEventArgs<T>> handler)
+    {
+      base.Subscribe(key, handler);
+    }
+
+    public new void Unsubscribe<T>(TKey key, EventHandler<DataReceivedEventArgs<T>> handler)
+    {
+      base.Unsubscribe(key, handler);
+    }
+
+    public new void Invoke<T>(TKey key, object sender, DataReceivedEventArgs<T> eventArgs)
+    {
+      base.Invoke(key, sender, eventArgs);
+    }
+  }
+}
