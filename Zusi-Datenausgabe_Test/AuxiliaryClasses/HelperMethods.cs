9e4c9901c977cf8628c2e256e25cbd19ecce70b9
AndreasKarg <Clonkman@gmx.de>
2013-07-31 16:42:45 +0200

    Test: Add test for simple SynchronizationContext

---
+++ Zusi-Datenausgabe_Test/AuxiliaryClasses/HelperMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zusi_Datenausgabe_Test.AuxiliaryClasses
+{
+  public static class HelperMethods
+  {
+    public static T ExpectException<T>(Action action) where T: Exception
+    {
+      try
+      {
+        action();
+      }
+      catch (Exception ex)
+      {
+        Assert.IsInstanceOfType(ex, typeof(T));
+        return ex as T;
+      }
+
+      Assert.Fail("No Exception thrown.");
+      return null;
+    }
+  }
+}
