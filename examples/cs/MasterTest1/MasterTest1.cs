e3d4dde7b79abb1eb00d1ddbee236fcb79503060
FSchn <fabian.schnorr@arcor.de>
2014-07-24 22:20:01 +0200

    Feature: Quirx-Mode for old TCPServer for the Master.

--- examples/cs/MasterTest1/MasterTest1.cs
+++ examples/cs/MasterTest1/MasterTest1.cs
@@ -6,6 +6,7 @@ class ServertTest1
 		System.Console.WriteLine("TCP Master");
 		var master = new Zusi_Datenausgabe.ZusiTcpTypeMaster("TestMaster", (System.Threading.SynchronizationContext) null);
 		System.Console.WriteLine("Set up Master successfull");
+		master.ErrorReceived += TCPConnection_ErrorReceived;
 		master.Connect("localhost", 1435);
 		while (master.ConnectionState != Zusi_Datenausgabe.ConnectionState.Connected) System.Threading.Thread.Sleep(100);
 		System.Console.WriteLine("Connecting successfull");
@@ -13,11 +14,18 @@ class ServertTest1
 		while(true) 
 		{
 			i ++;
-			System.Single v = (System.Single) (120 * System.Math.Sin((double) i / 360.0 * System.Math.PI));
-			System.Console.WriteLine(string.Format("v := {0} km/h", v));
+			double cosArg = (double) i / 360.0 * 2 * System.Math.PI;
+			System.Single v = (System.Single) (60 * (1 - System.Math.Cos(cosArg)));
+			System.Single a = (System.Single) (60 / 36 * System.Math.Sin(cosArg));
+			System.Console.WriteLine(string.Format("v := {0} km/h a := {1} m/s^2", v, a));
 			master.SendSingle(v, 2561);
+			master.SendSingle(a, 2657);
 			System.Threading.Thread.Sleep(100);
 			i = i % 360;
 		}
+	}
+	private static void TCPConnection_ErrorReceived(object sender, Zusi_Datenausgabe.ZusiTcpException ex)
+	{
+		System.Console.WriteLine(ex.ToString());
 	}
 }
