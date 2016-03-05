091987eee99c42252436e2be68471fb4ff018868
FSchn <fabian.schnorr@arcor.de>
2016-03-05 14:01:17 +0100

    Add: Example for protocol 3

---
+++ examples/cs/Client3Test1/Client3Test1.cs
@@ -0,0 +1,21 @@
+class ServertTest1
+{
+
+	public static void Main()
+	{
+		System.Console.WriteLine("TCP Client");
+		var client = new Zusi_Datenausgabe.ZusiTcp3TypeClient("Beispielclient", "1.0.0.0", 
+										(System.Threading.SynchronizationContext) null);
+		System.Console.WriteLine("Set up Client successfull");
+		client.RequestData(0x1);
+		client.Connect("localhost", 1436);
+		while (client.ConnectionState != Zusi_Datenausgabe.ConnectionState.Connected) System.Threading.Thread.Sleep(100);
+		System.Console.WriteLine("Connecting successfull");
+		client.FloatReceived += On_FloatReceived;
+		while(true) System.Threading.Thread.Sleep(100);
+	}
+	private static void On_FloatReceived(object sender, Zusi_Datenausgabe.DataSet<float> data)
+	{
+		System.Console.WriteLine(string.Format("v = {0} km/h", data.Value * 3.6));
+	}
+}
