712eafd6d943a27966f23c15e8146b71d9c738b3
Andreas Karg <andreas@bluefruit.co.uk>
2016-03-16 11:52:00 +0000

    Add simple command set parser

---
+++ ZusiTcpInterface/Zusi3/CommandsetReader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace ZusiTcpInterface.Zusi3
+{
+  internal static class CommandsetReader
+  {
+    public static IEnumerable<Command> ReadCommandsetFrom(Stream inputStream)
+    {
+      var commands = new List<Command>();
+      var streamReader = new StreamReader(inputStream);
+
+      while (!streamReader.EndOfStream)
+      {
+        var line = streamReader.ReadLine();
+
+        // ToDo: Make handling of syntax errors more graceful - e.g. w/ error message and continuation...
+        var columns = line.Split(';');
+        short id = short.Parse(columns[0].Split('x', 'X')[1], NumberStyles.HexNumber);
+        string name = columns[1];
+        string unit = columns[2];
+        string converter = columns[3];
+        string comment = (columns.Length == 5) ? columns[4] : "";
+
+        commands.Add(new Command(id, name, unit, converter, comment));
+      }
+
+      return commands;
+    }
+  }
+}
---
+++ ZusiTcpInterfaceTests/Zusi3/CommandsetReaderTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ZusiTcpInterface.Zusi3;
+
+namespace ZusiTcpInterfaceTests.Zusi3
+{
+  [TestClass]
+  public class CommandsetReaderTests
+  {
+    [TestMethod]
+    public void ReadsCsvCorrectly()
+    {
+      // Given
+      string commandsetCsv =
+@"0x0001;Geschwindigkeit;m/s;Single
+0x0002;Druck Hauptluftleitung;bar;Single
+0x0003;Druck Bremszylinder;bar;Single
+0x0004;Druck Hauptluftbehälter;bar;Single;Mit Sauce
+0x0005;Luftpresser läuft;aus/an;BoolAsSingle
+0x0006;Luftstrom Fvb;-1...0...1;Fail
+0x0007;Luftstrom Zbv;-1...0...1;Single
+0x0008;Lüfter an;aus/an;BoolAsSingle";
+
+      var expectedCommands = new List<Command>
+      {
+        new Command(0x01, "Geschwindigkeit", "m/s", "Single"),
+        new Command(0x02, "Druck Hauptluftleitung", "bar", "Single"),
+        new Command(0x03, "Druck Bremszylinder", "bar", "Single"),
+        new Command(0x04, "Druck Hauptluftbehälter", "bar", "Single", "Mit Sauce"),
+        new Command(0x05, "Luftpresser läuft", "aus/an", "BoolAsSingle"),
+        new Command(0x06, "Luftstrom Fvb", "-1...0...1", "Fail"),
+        new Command(0x07, "Luftstrom Zbv", "-1...0...1", "Single"),
+        new Command(0x08, "Lüfter an", "aus/an", "BoolAsSingle"),
+      };
+
+      var inputStream = new MemoryStream(Encoding.UTF8.GetBytes(commandsetCsv.ToCharArray()));
+
+      // When
+      var commands = CommandsetReader.ReadCommandsetFrom(inputStream).ToList();
+
+      // Then
+      foreach (var expectedCommand in expectedCommands)
+      {
+        Assert.IsTrue(commands.Contains(expectedCommand));
+      }
+    }
+  }
+}
