e03e00bee00b07678a1ecd8013db7b0e039c5f05
Andreas Karg <andreas@bluefruit.co.uk>
2016-05-28 16:18:45 +0100

    Add Address struct  (unused)

---
+++ ZusiTcpInterface/Zusi3/Address.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace ZusiTcpInterface.Zusi3
+{
+  public struct Address : IEquatable<Address>
+  {
+    private readonly int _hashValue;
+
+    public Address(params short[] ids)
+    {
+      _hashValue = ids.Aggregate(0, AddToHash);
+    }
+
+    private static int AddToHash(int hashValue, short id)
+    {
+      hashValue *= 367;
+      hashValue ^= id;
+
+      return hashValue;
+    }
+
+    public bool Equals(Address other)
+    {
+      return _hashValue == other._hashValue;
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (ReferenceEquals(null, obj)) return false;
+      if (obj.GetType() != this.GetType()) return false;
+      return Equals((Address) obj);
+    }
+
+    public override int GetHashCode()
+    {
+      return _hashValue;
+    }
+
+    public static bool operator ==(Address left, Address right)
+    {
+      return Equals(left, right);
+    }
+
+    public static bool operator !=(Address left, Address right)
+    {
+      return !Equals(left, right);
+    }
+  }
+}
---
+++ ZusiTcpInterfaceTests/Zusi3/AddressTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZusiTcpInterface.Zusi3;
+
+namespace ZusiTcpInterfaceTests.Zusi3
+{
+  [TestClass]
+  public class AddressTests
+  {
+    [TestMethod]
+    public void Identical_addresses_are_treated_as_equal()
+    {
+      // Given
+      var address1 = new Address(1, 2, 3);
+      var address2 = new Address(1, 2, 3);
+
+      // When
+      var areEqual = address1 == address2;
+      var areInequal = address1 != address2;
+
+      // Then
+      Assert.IsTrue(areEqual);
+      Assert.IsFalse(areInequal);
+      Assert.AreEqual(address1, address2);
+      Assert.IsTrue(address1.Equals(address2));
+    }
+  }
+}
